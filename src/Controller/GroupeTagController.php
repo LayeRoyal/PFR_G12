f8f9d9826ab04bade488b0fc39a36669d12986ee
adelino-bit <62213863+adelino-bit@users.noreply.github.com>
2020-08-14 17:37:06 +0000

    Les tag et les groupes de tags fin

--- src/Controller/GroupeTagController.php
+++ src/Controller/GroupeTagController.php
@@ -39,50 +39,35 @@ class GroupeTagController extends AbstractController
 
 
 
-    /**
-     * @Route(path="api/admin/grptags/{id}", methods={"put"})
-     */
-    public function putGroupeTag(TagRepository $repo, GroupeTagRepository $reposi, $id, Request $request, SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $manager )
-    {
-        $tag=$request->getContent();
-        $tab_grp_tag = $serializer->decode($tag,"json");
-        dd($tag);
-
-         $new_grp_tag= $reposi->find($id);
-         
-               
-                foreach ($tab_grp_tag["tags"] as $key => $value ) { 
-                $tag= $repo->findOneBy(['libelle' => $value]);
-                $new_grp_tag->addTag($tag);
-                }
-         
-        $manager->persist($new_grp_tag);
-        $manager->flush();
-        return $this->json($new_grp_tag,Response::HTTP_CREATED);
-
-    }
+    
 
 
      /**
      * @Route(path="api/admin/grptags/{id}", methods={"put"})
      */
-    public function putGroupeTag(TagRepository $repo, GroupeTagRepository $reposi, $id, Request $request, SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $manager )
+    public function putTag(TagRepository $repo, GroupeTagRepository $reposit, $id, Request $request, SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $manager )
     {
-        $tag=$request->getContent();
-        $tab_grp_tag = $serializer->decode($tag,"json");
-        dd($tag);
+        $grpe_tag=$reposit->find($id);
+        if(!$grpe_tag)
+        {
+            return $this ->json(["message" => "Groupe tag not found"], Response::HTTP_NOT_FOUND);
+        }
+       //supprimer ts les Tags pour ajouter ce qu'on veut
+        $tags=$grpe_tag->getTags();
+         foreach ($tags as $value) {
+            $grpe_tag->removeTag($value);
+         }
+       
+       $tags=$request->getContent();
+       $tab_tags = $serializer->decode($tags,"json");
 
-         $new_grp_tag= $reposi->find($id);
-         
-               
-                foreach ($tab_grp_tag["tags"] as $key => $value ) { 
-                $tag= $repo->findOneBy(['libelle' => $value]);
-                $new_grp_tag->removeTag($tag);
-                }
-         
-        $manager->persist($new_grp_tag);
-        $manager->flush();
-        return $this->json($new_grp_tag,Response::HTTP_CREATED);
+       foreach ($tab_tags['tags'] as $value) {
+        $tag=$repo->findOneBy(['libelle'=>$value]);
+        $grpe_tag->addTag($tag);
+    }
+     $manager->persist($grpe_tag);
+    $manager->flush();
+    return $this->json($grpe_tag,Response::HTTP_OK);
 
     }
 
